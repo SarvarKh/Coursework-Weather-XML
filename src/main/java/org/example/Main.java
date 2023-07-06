16b13d5349bccc971f8bbc8e4a3d62c0d9e6e5c8
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-07-06 22:26:44 +0500

    Add switch for processing options

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.controller.ConsoleController;
+import org.example.service.XMLService;
 
 public class Main {
     public static final String DATADIR = "src/main/resources/";
@@ -10,8 +11,23 @@ public class Main {
         // 0. Select options from Menu
         Integer optionNumber = ConsoleController.getOptionNumber();
 
-
-
+        // 1. Transform XML into another file based on XSL
+        switch (optionNumber) {
+            case 1:
+                System.out.println("Producing HTML reports...");
+                String xslHtmlName = "weather_html.xsl";
+                // Transform XML into HTML reports based on XSL
+                XMLService.transformXMLtoAnotherFileBasedOnXSL(xslHtmlName);
+                break;
+            case 2:
+                System.out.println("Producing XML reports...");
+                String xslXmlName = "weather_xml.xsl";
+                // Transform XML into another XML reports based on XSL
+                XMLService.transformXMLtoAnotherFileBasedOnXSL(xslXmlName);
+                break;
+            default:
+                System.out.println("Sorry, this option is not available currently");
+        }
 
         System.out.println("Ending application...\n-=-=-=-=-*****-=-=-=-=-");
     }
---
+++ src/main/java/org/example/service/XMLService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+public class XMLService {
+    public static void transformXMLtoAnotherFileBasedOnXSL(String xslFileName) {
+
+    }
+}
